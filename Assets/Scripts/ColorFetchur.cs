c72ba139267b58bc01847ac3576d392429140d24
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2024-03-08 11:56:46 -0400

    Move ColorFetchur to new file

---
+++ Assets/Scripts/ColorFetchur.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+static class ColorFetchur
+{
+    static Color[] colors = {
+        Color.HSVToRGB(0.6f, 0.9f, 0.9f),
+        Color.HSVToRGB(0.16f, 0.9f, 0.9f),
+        Color.HSVToRGB(0.08f, 1, 0.9f),
+        Color.HSVToRGB(0, 0.9f, 0.9f),
+        Color.HSVToRGB(0.33f, 0.9f, 0.9f)
+        };
+
+    public static Color GetColour(foeState state) { return GetColour((int)state); }
+    public static Color GetColour(int index) { return colors[index]; }
+}
